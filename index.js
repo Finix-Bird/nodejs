3521258dc35c1703a33a92edd991c7f24ea39b6e
Finix-Bird <mkarthickkumar40@gmail.com>
2023-10-03 11:51:25 +0530

    http,url module

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-//local module
-// const g=require('./calc')
-// const c=require('./one')
-// const co=require('./two')
+const http=require('http')
+// const url=require('url')
 
-// console.log(g.add(1,2))
-// console.log(g.n);
-// console.log(g.dummy());
+// const name="Karthi"
+// http.createServer((req,res)=>{
+//     res.end("<h1>Welcome "+name+"</h1>")
+// }).listen(4000)
 
-// const c1=new c.myClass("Karthi")
-// c1.myFun()
+const app=http.createServer((req,res)=>{
+    // res.write(req.url)
+    if(req.url==="/")
+    {
+        res.write("<h1>HOME</h1>")
+    }
+    if(req.url==="/about")
+    {
+        res.write("<h1>ABOUT</h1>")
+    }
+    if(req.url==="/product")
+    {
+        res.write("<h1>Our Services</h1>")
+    }
+    res.end()
+})
 
-// console.log(co.courseName);
-// console.log(co);
+
+app.listen(2500,()=>{console.log('Server started on 2500')})
